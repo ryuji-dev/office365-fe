9e14d2201468a568f710af73287b0851bea0ce82
Ryuji <hi_ryuji@naver.com>
2025-02-12 15:17:43 +0900

    feat: Enhance Terms Agreement functionality - 모든 동의 체크박스를 토글하는 로직 구현 - 필수 동의 체크박스가 체크되어야 회원가입 버튼 활성화 - 체크박스 상호작용으로 사용자 경험 개선

--- src/components/auth/TermsModal.tsx
+++ src/components/auth/TermsModal.tsx
@@ -1,66 +1,132 @@
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import { X } from 'lucide-react';
+import { Link, useNavigate } from 'react-router-dom';
 import Agreement from './Agreement';
 
 function TermsModal() {
+  const navigate = useNavigate();
+
+  const [checked, setChecked] = useState({
+    agreeAll: false,
+    agreeTerms: false,
+    agreePrivacy: false,
+    over14: false,
+  });
+
+  const handleAgreeAllChange = () => {
+    const newCheckedState = !checked.agreeAll;
+    setChecked({
+      agreeAll: newCheckedState,
+      agreeTerms: newCheckedState,
+      agreePrivacy: newCheckedState,
+      over14: newCheckedState,
+    });
+  };
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, checked } = event.target;
+    setChecked((prevState) => ({
+      ...prevState,
+      [id]: checked,
+    }));
+  };
+
+  const handleCancel = () => {
+    navigate('/login');
+  };
+
+  const handleSignUp = () => {
+    navigate('/signup');
+  };
+
+  const isSignUpDisabled = !(
+    checked.agreeTerms &&
+    checked.agreePrivacy &&
+    checked.over14
+  );
+
   return (
-    <div className="fixed inset-0 bg-[url('./src/assets/backgrounds/auth.png')] bg-cover bg-center bg-no-repeat flex justify-center items-center">
+    <div className="fixed inset-0 bg-zinc-900 bg-opacity-50 flex justify-center items-center">
       <div className="relative flex flex-col items-center justify-center gap-4 bg-gray-50 w-[25rem] h-[40rem] p-4 rounded-lg shadow-lg bg-opacity-80">
-        <Link to="/login" className="absolute top-2 right-2 cursor-pointer">
+        <Link to="/login" className="absolute top-2 right-2">
           <X />
         </Link>
-        <h1 className="text-3xl m-6 text-center text-zinc-900">약관동의</h1>
-        <div className="flex items-center mb-6">
-          <input
-            type="checkbox"
-            id="agreeAll"
-            className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
-          />
-          <label htmlFor="agreeAll" className="text-zinc-900 cursor-pointer">
-            이용약관, 개인정보 수집 및 이용에 모두 동의합니다.
-          </label>
-        </div>
-        <div className="flex items-center mb-2">
-          <input
-            type="checkbox"
-            id="agreeTerms"
-            className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
-          />
-          <label htmlFor="agreeTerms" className="text-zinc-900 cursor-pointer">
-            이용약관 동의 <span className="text-rose-500">(필수)</span>
-          </label>
-        </div>
-        <Agreement type="terms" />
-        <div className="flex items-center mb-2">
-          <input
-            type="checkbox"
-            id="agreePrivacy"
-            className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
-          />
-          <label
-            htmlFor="agreePrivacy"
-            className="text-zinc-900 cursor-pointer"
-          >
-            개인정보 수집 및 이용 동의{' '}
-            <span className="text-rose-500">(필수)</span>
-          </label>
-        </div>
-        <Agreement type="privacy" />
-        <div className="flex items-center mb-2">
-          <input
-            type="checkbox"
-            id="over14"
-            className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
-          />
-          <label htmlFor="over14" className="text-zinc-900 cursor-pointer">
-            만 14세 이상입니다. <span className="text-rose-500">(필수)</span>
-          </label>
+        <h2 className="text-3xl m-6 text-center text-zinc-900">약관동의</h2>
+        <div>
+          <div className="flex items-center mb-6">
+            <input
+              type="checkbox"
+              id="agreeAll"
+              checked={checked.agreeAll}
+              onChange={handleAgreeAllChange}
+              className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
+            />
+            <label htmlFor="agreeAll" className="text-zinc-900 cursor-pointer">
+              이용약관, 개인정보 수집 및 이용에 모두 동의합니다.
+            </label>
+          </div>
+          <div className="flex items-center mb-2">
+            <input
+              type="checkbox"
+              id="agreeTerms"
+              checked={checked.agreeTerms}
+              onChange={handleCheckboxChange}
+              className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
+            />
+            <label
+              htmlFor="agreeTerms"
+              className="text-zinc-900 cursor-pointer"
+            >
+              이용약관 동의 <span className="text-rose-500">(필수)</span>
+            </label>
+          </div>
+          <Agreement type="terms" />
+          <div className="flex items-center mb-2">
+            <input
+              type="checkbox"
+              id="agreePrivacy"
+              checked={checked.agreePrivacy}
+              onChange={handleCheckboxChange}
+              className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
+            />
+            <label
+              htmlFor="agreePrivacy"
+              className="text-zinc-900 cursor-pointer"
+            >
+              개인정보 수집 및 이용 동의{' '}
+              <span className="text-rose-500">(필수)</span>
+            </label>
+          </div>
+          <Agreement type="privacy" />
+          <div className="flex items-center mb-2">
+            <input
+              type="checkbox"
+              id="over14"
+              checked={checked.over14}
+              onChange={handleCheckboxChange}
+              className="mr-2 scale-150 accent-indigo-500 hover:accent-indigo-600 transition-all duration-300"
+            />
+            <label htmlFor="over14" className="text-zinc-900 cursor-pointer">
+              만 14세 이상입니다. <span className="text-rose-500">(필수)</span>
+            </label>
+          </div>
         </div>
         <div className="flex justify-between w-full px-14">
-          <button className="bg-gray-400 text-gray-50 px-10 py-3 rounded-lg hover:bg-gray-500 active:bg-gray-600 transition-all duration-300">
+          <button
+            onClick={handleCancel}
+            className="bg-gray-400 text-gray-50 px-10 py-3 rounded-lg hover:bg-gray-500 active:bg-gray-600 transition-all duration-300 cursor-pointer"
+          >
             취소
           </button>
-          <button className="px-7 py-3 rounded-lg transition-all duration-300 bg-indigo-500 text-gray-50 hover:bg-indigo-600 active:bg-indigo-700">
+          <button
+            onClick={handleSignUp}
+            disabled={isSignUpDisabled}
+            className={`px-7 py-3 rounded-lg transition-all duration-300 ${
+              isSignUpDisabled
+                ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+                : 'bg-indigo-500 text-gray-50 hover:bg-indigo-600 active:bg-indigo-700 cursor-pointer'
+            }`}
+          >
             회원가입
           </button>
         </div>
